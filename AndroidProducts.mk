fc16df3c4471f9941b0a1de45222e8ba2ba063a8
Martin <213996961+clexan@users.noreply.github.com>
2025-07-02 16:40:24 +0200

    gta9: Remove COMMON_LUNCH_CHOICES
    * generally deprecated, but to clean up a bit, we only build `-eng` on TWRP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,7 +8,4 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_gta9.mk
 
-COMMON_LUNCH_CHOICES := \
-    twrp_gta9-user \
-    twrp_gta9-userdebug \
-    twrp_gta9-eng
+COMMON_LUNCH_CHOICES := twrp_gta9-eng
