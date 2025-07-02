01b1711b40999bf7d85ba1d64d70804f3c18f88e
Martin <213996961+clexan@users.noreply.github.com>
2025-07-02 16:35:34 +0200

    gta9: cleanup: remove copyright years

--- twrp_gta9.mk
+++ twrp_gta9.mk
@@ -1,6 +1,6 @@
 #
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+# Copyright (C) The Android Open Source Project
+# Copyright (C) SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
